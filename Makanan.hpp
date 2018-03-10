936c7d830bc63e8136891d2c2e5a741e55da024d
AndreasHalim <32333567+AndreasHalim@users.noreply.github.com>
2018-03-10 23:01:16 +0700

    Create Makanan.hpp

---
+++ Makanan.hpp
@@ -0,0 +1,13 @@
+// Makanan.hpp
+/* Kelas Makanan merupakan turunan dari Kelas Entitas */
+/* Sejauh ini belum/tidak memiliki keturunan */
+
+#include <iostream>
+using namespace std;
+
+#include "Entitas.hpp"
+
+class Makanan: public Entitas {
+	public:
+		
+};
