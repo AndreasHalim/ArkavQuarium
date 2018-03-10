eeda6f4ab9dad3b8cddebb44566729764848adb5
AndreasHalim <32333567+AndreasHalim@users.noreply.github.com>
2018-03-10 23:02:23 +0700

    Create Piranha.hpp

---
+++ Piranha.hpp
@@ -0,0 +1,16 @@
+// Piranha.hpp
+/* Kelas Piranha merupakan turunan dari kelas Ikan */
+/* Sejauh ini belum/tidak memiliki keturunan */
+
+#include <iostream>
+using namespace std;
+
+#include "Ikan.hpp"
+
+class Piranha: public Ikan {
+	private:
+		
+	
+	public:
+	
+};
