146e3af47b249cbd651dfd7b26ba9f9947165856
AndreasHalim <32333567+AndreasHalim@users.noreply.github.com>
2018-03-10 23:01:53 +0700

    Create Guppy.hpp

---
+++ Guppy.hpp
@@ -0,0 +1,16 @@
+// Guppy.hpp
+/* Kelas Guppy merupakan turunan dari kelas Ikan */
+/* Sejauh ini belum/tidak memiliki keturunan */
+
+#include <iostream>
+using namespace std;
+
+#include "Ikan.hpp"
+
+class Guppy: public Ikan {
+	private:
+		
+	
+	public:
+	
+};
