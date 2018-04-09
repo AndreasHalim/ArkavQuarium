3ca51f804337be9651eae3bcd9d0e568ac2a7cde
Andre <32333567+AndreasHalim@users.noreply.github.com>
2018-04-09 17:18:42 +0700

    Entity.h dari Jose. Kelas abstrak pada prosedur MOVE

--- Entity.h
+++ Entity.h
@@ -1,25 +1,23 @@
+#include "Aquarium.h"
+
 class Entity {
-public:
-	Entity(int x,int y, int speed);
-	~Entity();
-	Entity(const Entity&);
-	Entity& operator=(const Entity&);
-
-	virtual void move(int x, int y);
-
-	int getSpeed();
-	int getX();
-	int getY();
-	int getId();
-
-	void setSpeed(int speed);
-	void setX(int x);
-	void setY(int y);
-	void setId(int id);
-private:
-	int speed;
-	int x;
-	int y;
-	int id;
-	static int count;
+	public:
+		Entity(int x, int y, int speed);
+		~Entity();
+		Entity(const Entity& E);
+		Entity& operator=(const Entity& E);
+		virtual void move(Aquarium& Aq, int x, int y) = 0;
+		int getSpeed() const;
+		int getX() const;
+		int getY() const;
+		int getId() const;
+		void setSpeed(int speed);
+		void setX(int x);
+		void setY(int y);
+	private:
+		int speed;
+		int x;
+		int y;
+		const int id;
+		static int count;
 }
